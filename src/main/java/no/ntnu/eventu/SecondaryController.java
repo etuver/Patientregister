2a498613479bfadd03acc7d945922240118bb8af
Even Tuverud <eventu@stud.ntnu.no>
2021-04-14 22:41:55 +0200

    Small changes to SecondaryController to test scenebuilder
    scenebuilder was bugging, Secondary used for testing purposes, not in use

--- src/main/java/no/ntnu/eventu/SecondaryController.java
+++ src/main/java/no/ntnu/eventu/SecondaryController.java
@@ -1,12 +1,20 @@
 package no.ntnu.eventu;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SecondaryController {
+
+    public Button secondaryButton;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
+    }
+
+    public void eplekake(ActionEvent actionEvent) {
     }
 }
