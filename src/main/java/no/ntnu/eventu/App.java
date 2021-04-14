24a4e355fd8fb98aa2fc7bd30cd50b2ec58aaed4
Even Tuverud <eventu@stud.ntnu.no>
2021-04-14 14:52:29 +0200

    Add title to the app
    The app now has the title "Patient register"

--- src/main/java/no/ntnu/eventu/App.java
+++ src/main/java/no/ntnu/eventu/App.java
@@ -4,6 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +24,8 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("Patient register");
+
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -34,5 +40,7 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 
 }
