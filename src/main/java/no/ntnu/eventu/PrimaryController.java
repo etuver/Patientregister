602f269b86698d589421e1a55802aac51fa9f8ed
Even Tuverud <eventu@stud.ntnu.no>
2021-04-14 22:44:50 +0200

    Remove PrimaryControllers autogenerated content from initial setup by intellij

--- src/main/java/no/ntnu/eventu/PrimaryController.java
+++ src/main/java/no/ntnu/eventu/PrimaryController.java
@@ -2,11 +2,23 @@ package no.ntnu.eventu;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
 
+
+
+
+
+
+
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public Button addPatientBtn;
+
+    @FXML
+    private  void initialize(){
+
+
+
     }
 }
