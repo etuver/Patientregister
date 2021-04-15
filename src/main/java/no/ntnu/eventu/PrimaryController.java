4f97787819ee99f91578632a5f5c3c80aa1a4883
Even Tuverud <eventu@stud.ntnu.no>
2021-04-15 11:53:11 +0200

    Add patientsTable and exitProgram alert
    Table is working with five columns, confirmed with test data
    Exit program confirmation alert, imports class Alert

--- src/main/java/no/ntnu/eventu/PrimaryController.java
+++ src/main/java/no/ntnu/eventu/PrimaryController.java
@@ -1,24 +1,124 @@
 package no.ntnu.eventu;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController {
 
 
+    //MenuItems
+    public MenuItem importMenu;
+    public MenuItem exportMenu;
+    public MenuItem exitMenu;
+    public MenuItem addMenu;
+    public MenuItem editMenu;
+    public MenuItem removeMenu;
+    public Menu helpMenu;
 
 
-
-
-
+    //Buttons
     @FXML
-    public Button addPatientBtn;
+    private Button editPatientBtn;
+    @FXML
+    private Button addPatientBtn;
+    @FXML
+    private Button removePatientBtn;
+    @FXML
+    private Button loadBtn;
+    @FXML
+    private Button saveBtn;
+
+    //Table
+    @FXML
+    private TableView<Patient> patientsTable;
+    @FXML
+    private TableColumn<Patient, String> ssn, firstName, lastName, diagnosis, generalPractitioner;
+
+
+
+
+
+
+
+    //Create a new PatientRegister
+    PatientRegister patientRegister = new PatientRegister("patientRegister");
+
+
+    /**
+     * Just some test data before implementing filehandling
+     */
+    public void fillWithTestData(){
+        patientRegister.registerPatient("16019112345", "Donald", "Trump", "Nobody knows", "A poor guy");
+        patientRegister.registerPatient("02019112345", "Mikke", "Mus", "Short tail", "Petter Smart");
+    }
+
+
+
 
     @FXML
     private  void initialize(){
+        fillWithTestData();
+
+        TableColumn<Patient, String> ssnCol = new TableColumn<>("Social Security Number");
+        ssnCol.setCellValueFactory(new PropertyValueFactory<>("ssn"));
+        patientsTable.getColumns().add(ssnCol);
+
+        TableColumn<Patient, String> firstNameCol = new TableColumn<>("First name");
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        patientsTable.getColumns().add(firstNameCol);
+
+        TableColumn<Patient, String> lastNameCol = new TableColumn<>("Last name");
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        patientsTable.getColumns().add(lastNameCol);
+
+
+        TableColumn<Patient, String> diagnosisCol = new TableColumn<>("Diagnosis");
+        diagnosisCol.setCellValueFactory(new PropertyValueFactory<>("diagnosis"));
+        patientsTable.getColumns().add(diagnosisCol);
+
+        TableColumn<Patient, String> gPCol = new TableColumn<>("General Practitioner");
+        gPCol.setCellValueFactory(new PropertyValueFactory<>("generalPractitioner"));
+        patientsTable.getColumns().add(gPCol);
+
+        //fills table
+        patientsTable.getItems().addAll(patientRegister.getPatients());
+
+
+
+        exitMenu.setOnAction(actionEvent -> exitProgram());
+
 
 
 
     }
+
+
+    /**
+     * Method to exit program
+     * imports class Alert to create a confirmation box before exiting
+     */
+    private void exitProgram(){
+        Alert exitAlert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to exit? \nAll unsaved progress will be lost!", ButtonType.YES, ButtonType.NO);
+        exitAlert.setTitle("Confirm exit");
+        exitAlert.setHeaderText("Exit application?");
+        exitAlert.setAlertType(Alert.AlertType.WARNING);
+        exitAlert.showAndWait();
+        if (exitAlert.getResult() == ButtonType.YES){
+            System.exit(0);
+        }
+    }
+
+
+
+
+
+
+
 }
