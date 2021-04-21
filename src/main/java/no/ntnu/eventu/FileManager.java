6bf3debe502bce1671ecf5fe16895c5e07575dc4
Even Tuverud <eventu@stud.ntnu.no>
2021-04-21 15:12:26 +0200

    File type validator for load and write header to csv
    Make method to control fileType when loading a file and show dialog if chosen file is not .csv.
    Also make writer write header to the csv file

--- src/main/java/no/ntnu/eventu/FileManager.java
+++ src/main/java/no/ntnu/eventu/FileManager.java
@@ -1,6 +1,7 @@
 package no.ntnu.eventu;
 
 
+import javafx.scene.control.Alert;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -13,6 +14,7 @@ import java.util.Scanner;
 
 /**
  * Class to manage writing and saving data from patientregister to file
+ * @author Eventu
  */
 public final class FileManager implements FileInterface {
 
@@ -20,35 +22,43 @@ public final class FileManager implements FileInterface {
     /**
      * Open file with register
      * Shows a FileChooser dialog to open file
+     * If file type is of csv or CSV, reads file and fille patientregister
+     * If file type is NOT csv or CSV, shows a dialog and return to primary window
      * @param patientRegister the patientregister which filedata will be added to
      * @throws FileNotFoundException
      */
     public void openFile(PatientRegister patientRegister) throws FileNotFoundException, IllegalArgumentException {
         FileChooser fileChooser = new FileChooser();
         File file = new File(fileChooser.showOpenDialog(new Stage()).getAbsolutePath());
-        //String filepath = file.getAbsolutePath();
-        Scanner scanner = new Scanner(new File(file.getAbsolutePath()));
-        while (scanner.hasNextLine()){
-            scanner.nextLine();
-            while (scanner.hasNextLine()){
-                String line = scanner.nextLine();;
-                String[] lineInfo = line.split(";");
-                String firstName = lineInfo[0];
-                String lastName = lineInfo[1];
-                String ssn = lineInfo[3];
-                String gpractitioner = lineInfo[2];
-                Patient p = new Patient(firstName,lastName,ssn,gpractitioner);
-                System.out.println(p.toString());
-                patientRegister.registerPatient(firstName,lastName,ssn,gpractitioner);
+        if (getFileExtension(file.getAbsoluteFile().toString()).equals("CSV") || getFileExtension(file.getAbsoluteFile().toString()).equals("csv")) { //Only reads if file is csv or CSV
+            Scanner scanner = new Scanner(new File(file.getAbsolutePath()));
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    ;
+                    String[] lineInfo = line.split(";");
+                    String firstName = lineInfo[0];
+                    String lastName = lineInfo[1];
+                    String ssn = lineInfo[3];
+                    String gpractitioner = lineInfo[2];
+                    patientRegister.registerPatient(firstName, lastName, ssn, gpractitioner);
+                }
             }
+            scanner.close();
+        } else {  //If file type is not csv
+            Alert errorLoadingAlert = new Alert(Alert.AlertType.ERROR);
+            errorLoadingAlert.setHeaderText("Invalid filetype chosen");
+            errorLoadingAlert.setContentText("Please choose a .csv file");
+            errorLoadingAlert.showAndWait();
         }
-        scanner.close();
-        }
+    }
 
 
     /**
      * Save the register to chosen file
      * Shows a FileChooser dialog to choose file
+     *
      * @param patientRegister the patientregister to save
      * @throws FileNotFoundException
      * @throws NullPointerException
@@ -56,13 +66,30 @@ public final class FileManager implements FileInterface {
     public void saveFile(PatientRegister patientRegister) throws FileNotFoundException, NullPointerException {
         FileChooser fileChooser = new FileChooser();
         File file = new File(fileChooser.showSaveDialog(new Stage()).getAbsolutePath());
-            PrintWriter writer = new PrintWriter(file);
-            for (int i = 0; i < patientRegister.getRegisterSize(); i++){
-                Patient p = patientRegister.getPatients().get(i);
-                writer.println(p.toString());
-            }
-            writer.flush();
-            writer.close();
+        PrintWriter writer = new PrintWriter(file);
+        writer.println("First Name" + ";" + "Last Name" + ";" + "General Practitioner" + ";" + "Social Security Number" + ";" + "Diagnosis");
+        for (int i = 0; i < patientRegister.getRegisterSize(); i++) {
+            Patient p = patientRegister.getPatients().get(i);
+            writer.println(p.toString());
+        }
+        writer.flush();
+        writer.close();
+    }
+
+
+    /**
+     * simple Method to check filetype of a name
+     *
+     * @param filename the filename to check, must include full filename with extension
+     * @return returns the extension of the file
+     */
+    private String getFileExtension(String filename) {
+        String extension = "";
+        int i = filename.lastIndexOf('.');
+        if (i >= 0) {
+            extension = filename.substring(i + 1);
+        }
+        return extension;
     }
 
 
