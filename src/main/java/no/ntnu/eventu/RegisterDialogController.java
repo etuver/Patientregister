66fcaced6af21e823cc01e273b22645f3dc9996f
Even Tuverud <eventu@stud.ntnu.no>
2021-04-26 18:36:51 +0200

    some javaDoc

--- src/main/java/no/ntnu/eventu/RegisterDialogController.java
+++ src/main/java/no/ntnu/eventu/RegisterDialogController.java
@@ -27,8 +27,8 @@ public class RegisterDialogController {
 
 
     @FXML
-    private void initialize(){
-        if (patientRegister.getRegisterSize() == 0){
+    private void initialize() {
+        if (patientRegister.getRegisterSize() == 0) {
             emptyRegWarningLbl.setText("NB! Your register is empty, you might want to load a file first");
         }
         doneRegisterBtn.setOnAction(actionEvent -> handleRegisterOk());
@@ -42,14 +42,19 @@ public class RegisterDialogController {
     }
 
 
-
-
-
-    public void handleRegisterOk(){
-      //  if (validInput() && patientRegister.ssnValidator(ssnText.getText())){
-             if (validInput()){
+    /**
+     * When ok button is pressed
+     * First checks if the input is valid. If invalid input, shows a Dialog box with which fields is invalid.
+     * If valid, Tries to register a patient, and gives the user a dialog to confirm it was successful.
+     * If registering the Patient was unsuccessfully, catches the exception and shows exceptionmessage in a alert dialog. Should
+     * normally not reach this point because of the validInput() first.
+     * <p>
+     * ValidInput first is to check all input fields at once to improve user experience instead of one dialog box for each invalid input
+     */
+    public void handleRegisterOk() {
+        if (validInput()) {
             try {
-                patientRegister.registerPatient(firstNameText.getText(), lastNameText.getText(),ssnText.getText(), gPText.getText());
+                patientRegister.registerPatient(firstNameText.getText(), lastNameText.getText(), ssnText.getText(), gPText.getText());
                 Alert okAlert = new Alert(Alert.AlertType.INFORMATION);
                 okAlert.setHeaderText("Success!");
                 okAlert.setContentText("Patient successfully registered");
@@ -59,7 +64,7 @@ public class RegisterDialogController {
                 okAlert.setGraphic(imageView);
                 okAlert.showAndWait();
                 switchToPrimary();
-            }catch (IllegalArgumentException | IOException e) {
+            } catch (IllegalArgumentException | IOException e) {
                 String errorMessage = e.getMessage();
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Invalid form");
@@ -67,11 +72,13 @@ public class RegisterDialogController {
                 alert.setContentText(errorMessage);
                 alert.showAndWait();
             }
-    }}
+        }
+    }
 
 
     /**
-     * Switch to primarycontroller
+     * Switch to primary controller
+     *
      * @throws IOException
      */
     @FXML
@@ -80,20 +87,30 @@ public class RegisterDialogController {
     }
 
 
-    private boolean validInput(){
+    /**
+     * Checks if the input from user is valid for a patient, and accumulates the field with wrong input to give the user a single feedback for all inputs
+     *
+     * @return true if all fields are valid, false if any input is wrong, and displays a dialog box with the errormessage
+     */
+    private boolean validInput() { // todo move to patientregister or patient?
         boolean valid = false;
         String errorMessage = "";
-        if (firstNameText.getText() == null || firstNameText.getText().length()  == 0){
+        if (firstNameText.getText() == null || firstNameText.getText().length() == 0) {
             errorMessage += "Invalid first name\n";
-        }if (lastNameText.getText() == null || lastNameText.getText().length()  == 0){
+        }
+        if (lastNameText.getText() == null || lastNameText.getText().length() == 0) {
             errorMessage += "Invalid last name\n";
-        }if (gPText.getText() == null || gPText.getText().length()  == 0) {
+        }
+        if (gPText.getText() == null || gPText.getText().length() == 0) {
             errorMessage += "Invalid General Practitioner\n";
-        }if (patientRegister.getPatientBySsn(ssnText.getText()) != null) {
+        }
+        if (patientRegister.getPatientBySsn(ssnText.getText()) != null) {
             errorMessage += "A patient with that social security number already exists!";
-        }if (!patientRegister.ssnValidator(ssnText.getText() )){
+        }
+        if (!patientRegister.ssnValidator(ssnText.getText())) {
             errorMessage += "Social security number is invalid";
-        }if (errorMessage.length() == 0){
+        }
+        if (errorMessage.length() == 0) {
             valid = true;
         } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -104,10 +121,6 @@ public class RegisterDialogController {
         }
         return valid;
     }
-
-
-
-
 
 
 }
